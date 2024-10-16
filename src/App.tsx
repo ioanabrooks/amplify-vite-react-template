0875e8cda33ba7df19dcb31df1d5b1ce60435c59
Ioana Brooks <68251134+ioanabrooks@users.noreply.github.com>
2024-10-16 09:29:16 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
+        🥳 App successfully hosted and updated, Rev 2.0. Try creating a new todo.
         <br />
         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
           Review next step of this tutorial.
